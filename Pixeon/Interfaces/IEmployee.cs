47a9015b89c9b78e47c8edc7215c64b290faa064
Garcia <garciasholding@gmail.com>
2023-03-01 17:18:08 -0300

    fix: cleaning unnecessary imports

--- Pixeon/Interfaces/IEmployee.cs
+++ Pixeon/Interfaces/IEmployee.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pixeon.Interfaces
+﻿namespace Pixeon.Interfaces
 {
     public interface IEmployee
     {
--- Pixeon/Interfaces/ITeam.cs
+++ Pixeon/Interfaces/ITeam.cs
@@ -1,17 +1,13 @@
 ﻿using Pixeon.Classes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Pixeon.Interfaces
 {
     public interface ITeam
     {
         string Name { get; }
-        int MinMaturity { get; }
-        int CurrentMaturity { get; }
+        static int MinMaturity { get; }
+        static int CurrentMaturity { get; }
         List<Employee> TeamEmployees { get; }
     }
 }
