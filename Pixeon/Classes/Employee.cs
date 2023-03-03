e1b0bff4f0f066ebffb75fae527f97a5a7880728
Garcia <garciasholding@gmail.com>
2023-03-03 14:06:49 -0300

    fix: access to properties

--- Pixeon/Classes/Employee.cs
+++ Pixeon/Classes/Employee.cs
@@ -4,10 +4,10 @@ namespace Pixeon.Classes
 {
     public class Employee : IEmployee
     {
-        public string Name { get; set; }
+        public string Name { get; private set; }
         public byte PLevel { get; set; }
-        public int BirthYear { get;  set; }
-        public int AdmissionYear { get;  set; }
+        public int BirthYear { get; private set; }
+        public int AdmissionYear { get; private set; }
         public int LastProgressionYear { get; set; }
         public float TotalScoreForPromotion { get; set; }
         public Employee(string name, byte pLevel, int birthYear, int admissionYear, int lastProgressionYear)
@@ -20,7 +20,7 @@ namespace Pixeon.Classes
             TotalScoreForPromotion = (float) GetTotalPromotionScore(admissionYear, lastProgressionYear, birthYear);
         }
 
-        static double GetTotalPromotionScore(int admissionYear, int lastProgressionYear, int birthYear)
+        public static double GetTotalPromotionScore(int admissionYear, int lastProgressionYear, int birthYear)
         {
             int currentYear = DateTime.Now.Year;
             int timeWithoutProgression = currentYear - lastProgressionYear;
--- Pixeon/Classes/Team.cs
+++ Pixeon/Classes/Team.cs
@@ -5,7 +5,7 @@ namespace Pixeon.Classes
     public class Team : ITeam
     {
         public string Name { get; private set; }
-        public int MinMaturity { get; set; }
+        public int MinMaturity { get; private set; }
         public int CurrentMaturity { get; set; }
         public List<Employee> TeamEmployees { get; set; } = new();
         public int ExtraMaturity { get; set; }   
@@ -17,7 +17,7 @@ namespace Pixeon.Classes
             ExtraMaturity = GetExtraMaturityPoints(CurrentMaturity, MinMaturity);
         }
 
-        static int GetExtraMaturityPoints(int currentMaturity, int minMaturity)
+        public static int GetExtraMaturityPoints(int currentMaturity, int minMaturity)
         { 
             return currentMaturity - minMaturity; 
         }
